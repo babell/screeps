d1f4eb79e308d3b1b6ca0bf902c9494daa312fed
Ben Abell <ben@abell.at>
2016-07-10 16:43:03 +0200

    added towerSupplier

--- structure.tower.js
+++ structure.tower.js
@@ -2,12 +2,13 @@ module.exports = {
     /** @param {Structure} structure **/
     run: function(structure) {
 
-        var closestDamagedStructure = structure.pos.findClosestByRange(FIND_STRUCTURES, {
+/**        var closestDamagedStructure = structure.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
         if(closestDamagedStructure) {
             structure.repair(closestDamagedStructure);
         }
+ **/
 
         var closestHostile = structure.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if(closestHostile) {
